7d0dff9ca700cd79258d51443061040a23c38f1b
MA Miller <miller.m.43@gmail.com>
2023-10-04 22:13:51 -0400

    add test for method not allowed

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -21,3 +21,20 @@ def test_home_page():
         assert b"Generate Playoff Odds Prediction Table" in response.data
         assert b"Team Historical Elo" in response.data
         assert b"Team Historical SRS" in response.data
+
+def test_home_page_post():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is posted to (POST)
+    THEN check that a '405' (Method Not Allowed) status code is returned
+    """
+    # Set the Testing configuration prior to creating the Flask application
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/')
+        assert response.status_code == 405
+        assert b"Navigation" not in response.data
+
