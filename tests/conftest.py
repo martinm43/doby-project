15c9bc39b6e862eaa84a32c94f5a61b824a33b72
MA Miller <miller.m.43@gmail.com>
2023-10-04 22:24:17 -0400

    basic fixtures for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import pytest
+from doby import create_app
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    # Set the Testing configuration prior to creating the Flask application
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context
+        with flask_app.app_context():
+            yield testing_client  
