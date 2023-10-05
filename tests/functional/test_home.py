3ff70a67d45033d75e23dfcb127ad2f6489c7309
MA Miller <miller.m.43@gmail.com>
2023-10-04 21:39:59 -0400

    First functional test

---
+++ tests/functional/test_home.py
@@ -0,0 +1,23 @@
+from doby import create_app
+import os
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response has all the expected links
+    """
+    # Set the Testing configuration prior to creating the Flask application
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = create_app()
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        print(response.data)
+        assert response.status_code == 200
+        assert b"Plot Season Playoff Odds" in response.data
+        assert b"Get Information About Season Between Dates" in response.data
+        assert b"Generate Playoff Odds Prediction Table" in response.data
+        assert b"Team Historical Elo" in response.data
+        assert b"Team Historical SRS" in response.data
